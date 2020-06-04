da321c44fc1cb854664d50417ca79f48e568a190
otmanerahim <otmane.rahim@etu.unice.fr>
2020-06-05 00:43:57 +0200

    #54 #49 ajout texture et controle mouvement
    + On peut controler les mouvements avec z,s,q,d plus besoin de souris

--- behaviour-experimentation/GameDemo/game.js
+++ behaviour-experimentation/GameDemo/game.js
@@ -17,9 +17,11 @@ var UiPanel;
 var UiPanelEditMap;
 var targets = []
 var pathBehaviours = [];
-var vehiclePlayer=undefined;
+var vehiclePlayer = undefined;
 var decelerate;
-var raceStarted=false;
+var raceStarted = false;
+var howToPlay;
+var infoVehicle;
 var concMap;
 var map;
 var colorVectors = {
@@ -41,15 +43,14 @@ var paramsGUI = [
 ]
 
 var createMap = (arrayMap) => {
-
     var concMap = []
-    for(let i=0;i<arrayMap.length;i++){
+    for (let i = 0; i < arrayMap.length; i++) {
 
-        var road = new Road(arrayMap[i].height,arrayMap[i].position,arrayMap[i].direction)
+        var road = new Road(arrayMap[i].height, arrayMap[i].position, arrayMap[i].direction)
         road.pathPoint = arrayMap[i].pathPoint
         road.createRoad(scene)
-        arrayMap[i].pathPoint.forEach(point=>{
-            concMap.push({"direction": arrayMap[i].direction,"path":new BABYLON.Vector3(point.x,point.y,point.z)});
+        arrayMap[i].pathPoint.forEach(point => {
+            concMap.push({ "direction": arrayMap[i].direction, "path": new BABYLON.Vector3(point.x, point.y, point.z) });
 
         });
     }
@@ -64,16 +65,32 @@ var createScene = function () {
 
     var scene = new BABYLON.Scene(engine);
 
-    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 3000, 60), scene);
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 2000, 60), scene);
     camera.setTarget(BABYLON.Vector3.Zero());
 
     camera.attachControl(canvas, true);
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    var gridMaterial = new BABYLON.GridMaterial("grid", scene);
-    ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 3500, height: 3500 }, scene);
-    gridMaterial.gridRatio = 10
+    var terrainMaterial = new BABYLON.TerrainMaterial("terrainMaterial", scene);
+    terrainMaterial.specularColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+    terrainMaterial.specularPower = 64;
+
+    terrainMaterial.mixTexture = new BABYLON.Texture("../resources/mixMap.png", scene);
+
+
+    terrainMaterial.diffuseTexture1 = new BABYLON.Texture("../resources/grass.png", scene);
+    terrainMaterial.diffuseTexture2 = new BABYLON.Texture("../resources/grass.png", scene);
+    terrainMaterial.diffuseTexture3 = new BABYLON.Texture("../resources/grass.png", scene);
+
+    // Bump textures according to the previously set diffuse textures
+    terrainMaterial.bumpTexture1 = new BABYLON.Texture("../resources/grassn.png", scene);
+    terrainMaterial.bumpTexture2 = new BABYLON.Texture("../resources/grassn.png", scene);
+    terrainMaterial.bumpTexture3 = new BABYLON.Texture("../resources/grassn.png", scene);
+
+    ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 3000, height: 3000 }, scene);
+
+    ground.material = terrainMaterial;
 
 
     var materialShip = new BABYLON.StandardMaterial("shiptx1", scene);
@@ -107,13 +124,13 @@ var createScene = function () {
     // BUTTONS TO STOP / START / SELECT ENTITIES
     var buttonStart = GUI.createButton("Start new entity", "10px", "100px", "100px", "white", "green");
     var buttonStop = GUI.createButton("Stop all entities", "10px", "100px", "100px", "white", "red");
-    
+
     var buttonStartRace = GUI.createButton("Start the race", "10px", "100px", "100px", "white", "green")
-    buttonStartRace.isEnabled=false;
-    
+    buttonStartRace.isEnabled = false;
+
 
     var buttonStopRace = GUI.createButton("Stop the race", "10px", "100px", "100px", "white", "red")
-    buttonStopRace.isEnabled=false;
+    buttonStopRace.isEnabled = false;
 
     UiPanel.addControl(buttonStart)
     UiPanel.addControl(buttonStop)
@@ -123,14 +140,14 @@ var createScene = function () {
         var target;
         var entity;
 
-        
+
         entity = BABYLON.Mesh.CreateCylinder("entity", 2, 0, 1, 6, 1, scene, false);
         entity.scaling = new BABYLON.Vector3(20, 20, 20)
         entity.material = materialShip;
         entity.checkCollisions = true
         entity.position.z = concMap[0].path.z
         entity.position.x = concMap[0].path.x - 100
-        
+
         var vehicle = new Vehicle(entity)
 
         target = BABYLON.Mesh.CreateCylinder("target", 2, 0, 1, 6, 1, scene, false);
@@ -149,7 +166,7 @@ var createScene = function () {
         vehicle.mass = paramsGUI[2].anim.toFixed(2)
         vehicle.desiredSeparation = paramsGUI[3].anim.toFixed(2)
 
-        
+
         vehicleTarget.maxSpeed = paramsGUI[0].anim.toFixed(2)
         vehicleTarget.maxForce = paramsGUI[1].anim.toFixed(2)
         vehicleTarget.mass = paramsGUI[2].anim.toFixed(2)
@@ -164,7 +181,7 @@ var createScene = function () {
 
         nameEntities.push(xChar);
 
-        
+
         //Vector of seek behaviours
         var decorMaxSpeed = new DecorVector(vehicle.mesh.position, 100, scene)
         var decorMaxForce = new DecorVector(vehicle.mesh.position, 100, scene)
@@ -182,12 +199,12 @@ var createScene = function () {
         pathBehaviours.push(pathBehaviour)
 
         entitiesCreated = true;
-        buttonStartRace.isEnabled=true;
-        
-        
-        camera.setTarget(entities[entities.length-1].mesh.position)
-            
-        camera.position = new BABYLON.Vector3(100,200,600)
+        buttonStartRace.isEnabled = true;
+
+
+        camera.setTarget(entities[entities.length - 1].mesh.position)
+
+        camera.position = new BABYLON.Vector3(1200, 200, 1600)
 
         //Update the checkbox GUI 
         checkboxGUI.forEach(child => {
@@ -220,7 +237,7 @@ var createScene = function () {
             })
         }
 
-        raceStarted=false;
+        raceStarted = false;
 
         entities = []
         targets = []
@@ -291,14 +308,22 @@ var createScene = function () {
     UiPanelEditMap.addControl(buttonStopRace)
 
     buttonStopRace.onPointerDownObservable.add(function () {
-        raceStarted=false;
-        entities.forEach(entity=>{
-            entity.dispose()
-        })
+        raceStarted = false;
+        for (let i = 0; i < entities.length; i++) {
+            targets[i].mesh.dispose()
+            entities[i].mesh.dispose()
+            nameEntities[i].dispose()
+        }
 
         vehiclePlayer.mesh.dispose()
-        camera.position= new BABYLON.Vector3(0,3000,60)
+        vehiclePlayer = undefined;
+        scene.actionManager.dispose()
+        scene.actionManager = undefined;
+        camera.position = new BABYLON.Vector3(0, 3000, 60)
         camera.setTarget(BABYLON.Vector3.Zero())
+        UiPanel.isVisible = true;
+        buttonStartRace.isVisible = true;
+        infoVehicle.isVisible=false;
 
     });
 
@@ -309,14 +334,15 @@ var createScene = function () {
         entity = BABYLON.Mesh.CreateCylinder("entity", 2, 0, 1, 6, 1, scene, false);
         entity.scaling = new BABYLON.Vector3(20, 20, 20)
         entity.material = materialShip.clone();
-        entity.material.diffuseColor = new BABYLON.Color3(0,1,0)
+        entity.material.diffuseColor = new BABYLON.Color3(0, 1, 0)
         entity.checkCollisions = true
         entity.position.z = concMap[0].path.z
         entity.position.x = concMap[0].path.x - 100
 
+        infoVehicle.isVisible=true;
         vehiclePlayer = new Vehicle(entity)
-        vehiclePlayer.maxSpeed = 12
-        vehiclePlayer.maxForce = 30
+        vehiclePlayer.maxSpeed = 10
+        vehiclePlayer.maxForce = 50
         vehiclePlayer.mass = 20
         vehiclePlayer.position.y = 8
 
@@ -327,57 +353,113 @@ var createScene = function () {
                 parameter: "z"
             },
             function () {
+
                 var forward = vehiclePlayer.position.subtract(camera.position).normalize();
-                forward.scaleInPlace(vehiclePlayer.maxSpeed)
                 forward.y = 0
-                vehiclePlayer.applyForce(forward)
+
+                var diffAngle = Math.atan2(forward.x, forward.z);
+                var currentAngle = diffAngle;
+                vehiclePlayer.mesh.rotation.y = currentAngle + (Math.PI);
+                var force = BABYLON.Vector3.TransformCoordinates(new BABYLON.Vector3(0, 0, -0.5 * vehiclePlayer.maxSpeed), BABYLON.Matrix.RotationY(vehiclePlayer.mesh.rotation.y));
+                vehiclePlayer.applyForce(force)
+                vehiclePlayer.rotate()
             }
         ));
 
         scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
             {
-                trigger: BABYLON.ActionManager.OnKeyUpTrigger,
-                parameter: "z"
+                trigger: BABYLON.ActionManager.OnKeyDownTrigger,
+                parameter: "d"
             },
             function () {
 
-                decelerate = setInterval(() => {
+                var forward = vehiclePlayer.position.subtract(camera.position).normalize();
+                forward.y = 0
+                var force = BABYLON.Vector3.TransformCoordinates(new BABYLON.Vector3(0, 0, 1 * vehiclePlayer.maxSpeed), BABYLON.Matrix.RotationY(vehiclePlayer.mesh.rotation.y));
 
-                    if (vehiclePlayer.velocity.subtract(new BABYLON.Vector3(0.1, 0, 0.1)).length() > 0) {
-                        if (vehiclePlayer.velocity.x > 0 && vehiclePlayer.velocity.z < 0) {
-                            vehiclePlayer.velocity.subtractInPlace(new BABYLON.Vector3(0.1, 0, -0.1));
-                        } else if (vehiclePlayer.velocity.x < 0 && vehiclePlayer.velocity.z > 0) {
+                vehiclePlayer.applyForce(force)
+                vehiclePlayer.rotate()
 
-                            vehiclePlayer.velocity.subtractInPlace(new BABYLON.Vector3(-0.1, 0, 0.1));
-                        } else if (vehiclePlayer.velocity.x > 0 && vehiclePlayer.velocity.z > 0) {
-
-                            vehiclePlayer.velocity.subtractInPlace(new BABYLON.Vector3(0.1, 0, 0.1));
-                        } else {
-
-                            vehiclePlayer.velocity.subtractInPlace(new BABYLON.Vector3(-0.1, 0, -0.1));
-                        }
-
-                        if (vehiclePlayer.velocity.length() < 0.3) {
-                            vehiclePlayer.velocity = new BABYLON.Vector3(0, 0, 0)
-                            clearInterval(decelerate)
-                            decelerate = undefined
-                        }
-                    }
-
-
-                }, 50)
             }
         ));
 
-        
-        camera.position = new BABYLON.Vector3(100, 200, 600);
+        scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
+            {
+                trigger: BABYLON.ActionManager.OnKeyDownTrigger,
+                parameter: "q"
+            },
+            function () {
 
-        raceStarted=true;
-        UiPanel.isVisible=false;
-        buttonLoad.isVisible=false;
-        buttonStartRace.isVisible=false;
-        buttonStopRace.isEnabled=true;
-        buttonStopRace.isVisible=true;
+                var forward = vehiclePlayer.position.subtract(camera.position).normalize();
+
+                forward.y = 0
+                var force = BABYLON.Vector3.TransformCoordinates(new BABYLON.Vector3(0, 0, -1 * vehiclePlayer.maxSpeed), BABYLON.Matrix.RotationY(vehiclePlayer.mesh.rotation.y));
+
+                vehiclePlayer.applyForce(force)
+                vehiclePlayer.rotate()
+
+            }
+        ));
+
+        scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(
+            {
+                trigger: BABYLON.ActionManager.OnKeyDownTrigger,
+                parameter: "s"
+            },
+            function () {
+
+                if (vehiclePlayer.velocity.length() < 1) {
+                    vehiclePlayer.velocity = new BABYLON.Vector3(0, 0, 0)
+                }else{
+                    if (vehiclePlayer.velocity.x > 0 && vehiclePlayer.velocity.z < 0) {
+                        vehiclePlayer.velocity.subtractInPlace(new BABYLON.Vector3(0.1 * vehiclePlayer.maxSpeed, 0, -0.1 * vehiclePlayer.maxSpeed));
+                    } else if (vehiclePlayer.velocity.x < 0 && vehiclePlayer.velocity.z > 0) {
+    
+                        vehiclePlayer.velocity.subtractInPlace(new BABYLON.Vector3(-0.1 * vehiclePlayer.maxSpeed, 0, 0.1 * vehiclePlayer.maxSpeed));
+                    } else if (vehiclePlayer.velocity.x > 0 && vehiclePlayer.velocity.z > 0) {
+    
+                        vehiclePlayer.velocity.subtractInPlace(new BABYLON.Vector3(0.1 * vehiclePlayer.maxSpeed, 0, 0.1 * vehiclePlayer.maxSpeed));
+                    } else {
+    
+                        vehiclePlayer.velocity.subtractInPlace(new BABYLON.Vector3(-0.1 * vehiclePlayer.maxSpeed, 0, -0.1 * vehiclePlayer.maxSpeed));
+                    }
+    
+
+                }
+
+
+            }
+
+
+        ));
+
+
+
+
+
+
+
+        camera.position = new BABYLON.Vector3(1200, 200, 1600)
+
+        raceStarted = true;
+        UiPanel.isVisible = false;
+        buttonLoad.isVisible = false;
+        buttonStartRace.isVisible = false;
+        buttonStopRace.isEnabled = true;
+        buttonStopRace.isVisible = true;
+
+        howToPlay.isVisible = true;
+
+
+        var buttonHowToPlay = GUI.createButton("GOT IT !", "10px", "50px", "50px", "white", "green");
+
+        buttonHowToPlay.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        howToPlay.addControl(buttonHowToPlay)
+
+        buttonHowToPlay.onPointerDownObservable.add(function () {
+
+            howToPlay.isVisible = false;
+        });
 
 
 
@@ -386,41 +468,90 @@ var createScene = function () {
 
     buttonLoad.onPointerDownObservable.add(function () {
 
-        
-        Utilities.readTextFile("./mapRace.json", function(text){
+
+        Utilities.readTextFile("./mapRace.json", function (text) {
             map = JSON.parse(text);
             concMap = createMap(map)
             var pointsPath = [];
-            concMap.forEach(conc=>{
+            concMap.forEach(conc => {
                 pointsPath.push(conc.path)
 
             });
-            var track = BABYLON.MeshBuilder.CreateLines("path", {points: pointsPath}, scene); 
-            track.position.y=8   
-            
+            var track = BABYLON.MeshBuilder.CreateLines("path", { points: pointsPath }, scene);
+            track.position.y = 8
+
         });
 
-        buttonStart.isEnabled=true;
-        
+        buttonStart.isEnabled = true;
 
-        camera.position = new BABYLON.Vector3(0,3000,60)
-        camera.setTarget(new BABYLON.Vector3(0,0,0));
-        buttonLoad.isEnabled=false;
+
+        camera.position = new BABYLON.Vector3(0, 3000, 60)
+        camera.setTarget(new BABYLON.Vector3(0, 0, 0));
+        buttonLoad.isEnabled = false;
 
     });
 
 
 
+    howToPlay = new BABYLON.GUI.Rectangle();
+
+    howToPlay.isVisible = false;
+    howToPlay.width = "0.4";
+    howToPlay.height = "200px";
+    howToPlay.cornerRadius = 20;
+    howToPlay.color = "Orange";
+
+    howToPlay.thickness = 4;
+    howToPlay.background = "green";
+    advancedTexture.addControl(howToPlay);
+
+
+    var howToPlayText = new BABYLON.GUI.TextBlock();
+    howToPlayText.text = "Accelerate : 'z' \n Decelerate : 's' \n Move Left : 'q' \n Move right : 'd' "
+    howToPlayText.marginRight = "5px";
+    howToPlayText.fontWeight = "bold"
+    howToPlayText.color = "white"
+    howToPlayText.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+    howToPlay.addControl(howToPlayText)
+
+    infoVehicle = new BABYLON.GUI.Rectangle();
+
+    infoVehicle.isVisible = false;
+    infoVehicle.width = "0.8";
+    infoVehicle.height = "200px";
+    infoVehicle.paddingTop="30px"
+    infoVehicle.cornerRadius = 20;
+    infoVehicle.color = "Orange";
+    
+    infoVehicle.thickness = 4;
+    infoVehicle.background = "green";
+    infoVehicle.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+
+    var vehicleMapSpeedText = new BABYLON.GUI.TextBlock();
+    vehicleMapSpeedText.text = " current speed : " +"\n";
+    vehicleMapSpeedText.marginRight = "5px";
+    vehicleMapSpeedText.fontWeight = "bold"
+    vehicleMapSpeedText.color = "white"
+    vehicleMapSpeedText.textVerticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+    infoVehicle.addControl(vehicleMapSpeedText)
+
+    
+    UiPanelEditMap.addControl(infoVehicle)
+
+
+
     scene.registerAfterRender(function () {
 
-        if(vehiclePlayer!==undefined){
+        if (vehiclePlayer !== undefined) {
 
             camera.setTarget(vehiclePlayer.mesh.position)
-            vehiclePlayer.rotate()
+            vehicleMapSpeedText.text = " current speed  : " +"\n" + vehiclePlayer.velocity.length().toFixed(2);
+        
+            //vehiclePlayer.rotate()
             vehiclePlayer.update()
         }
 
-        if (entitiesCreated === true && raceStarted===true) {
+        if (entitiesCreated === true && raceStarted === true && howToPlay.isVisible !== true) {
             for (let i = 0; i < entities.length; i++) {
                 // Update entities
 
@@ -430,7 +561,7 @@ var createScene = function () {
                 entities[i].applyBehaviour(pathBehaviours[i])
                 entities[i].update()
 
-                
+
                 // Update targets
                 var directionRotation = (targets[i].velocity.clone()).normalize()
                 directionRotation = Math.atan2(directionRotation.z, -directionRotation.x)
